0c2b237b526fada9168d851d48d62dd7407f065a
bartyomi <148716654+bartyomi@users.noreply.github.com>
2024-03-31 23:28:12 +0300

    Create main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,15 @@
+#include "../include/Automata.h"
+
+int main() {
+    Automata machine;
+    machine.on();
+    machine.coin(25);
+    machine.coin(50);
+    machine.choice("Cappuccino");
+    machine.check();
+    machine.cook();
+    machine.coin(50);
+    machine.cook();
+    machine.finish();
+    machine.off();
+}
