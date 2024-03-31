6919f8081c1ce09a7049d4d12772531d678e9941
bartyomi <148716654+bartyomi@users.noreply.github.com>
2024-03-31 23:36:46 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,84 @@
 // Copyright 2022 GHA Test Team
 
 #include <gtest/gtest.h>
-#include "Automata.h"
+#include "../include/Automata.h"
+
+TEST(test, on) {
+    Automata machine;
+    machine.on();
+    EXPECT_EQ(STATE::WAIT, machine.getState());
+}
+
+TEST(test, incorrectAction) {
+    Automata machine;
+    machine.coin(50);
+    EXPECT_EQ(STATE::WAIT, machine.getState());
+}
+
+TEST(test, off) {
+    Automata machine;
+    machine.on();
+    machine.off();
+    EXPECT_EQ(STATE::OFF, machine.getState());
+}
+
+TEST(test, coin) {
+    Automata machine;
+    machine.on();
+    machine.coin(20);
+    EXPECT_EQ(STATE::ACCEPT, machine.getState());
+}
+
+TEST(test, notEnoughMoney) {
+    Automata machine;
+    machine.on();
+    machine.coin(30);
+    machine.choice("Cappuccino");
+    EXPECT_EQ(false, machine.check());
+}
+
+TEST(test, enoughMoney) {
+    Automata machine;
+    machine.on();
+    machine.coin(100);
+    machine.choice("Cappuccino");
+    EXPECT_EQ(true, machine.check());
+}
+
+TEST(test, cancel) {
+    Automata machine;
+    machine.on();
+    machine.coin(200);
+    machine.cancel();
+    EXPECT_EQ(STATE::WAIT, machine.getState());
+}
+
+TEST(test, cook) {
+    Automata machine;
+    machine.on();
+    machine.coin(100);
+    machine.choice("Latte");
+    machine.cook();
+    EXPECT_EQ(STATE::COOK, machine.getState());
+}
+
+TEST(test, finish) {
+    Automata machine;
+    machine.on();
+    machine.coin(100);
+    machine.choice("Latte");
+    machine.cook();
+    machine.finish();
+    EXPECT_EQ(STATE::WAIT, machine.getState());
+}
+
+TEST(test, cancel2) {
+    Automata machine;
+    machine.on();
+    machine.coin(100);
+    machine.choice("Cappuccino");
+    machine.cancel();
+    machine.coin(100);
+    machine.choice("Espresso");
+    EXPECT_EQ(STATE::CHECK, machine.getState());
+}
